628f42f93ad7473c8494b7296520ea26d01b8621
love-to-code108 <love.to.code108@gmail.com>
2023-06-28 22:40:27 +0530

    bad save may be the root of all problems

---
+++ src/js/script1.js
@@ -0,0 +1,7 @@
+console.log("Its working down here");
+
+
+const button = document.getElementById("landBtn");
+landBtn.document.addEventListener("click", helloWorld =>{
+    console.log("am i alive");
+});
