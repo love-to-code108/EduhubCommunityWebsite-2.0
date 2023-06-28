00adf753d4fe9e4170771e173ca7868676c82a6d
love-to-code108 <love.to.code108@gmail.com>
2023-06-28 20:45:18 +0530

    save 4 custom mouse donegit add .

---
+++ src/js/customCursor.js
@@ -0,0 +1,29 @@
+// ==============================================
+// the inner mouse
+{
+    const c = document.getElementById("cursorInner");
+    document.addEventListener("mousemove", mouseHalo => {
+        const posX = mouseHalo.clientX;
+        const posY = mouseHalo.clientY;
+
+        c.style.left = `${posX}px`;
+        c.style.top = `${posY}px`;
+    })
+}
+
+
+
+
+// ===============================================
+// the outer mouse
+
+{
+    const c = document.getElementById("cursorOuter");
+    document.addEventListener("mousemove", mouseHalo => {
+        const posX = mouseHalo.clientX;
+        const posY = mouseHalo.clientY;
+
+        c.style.left = `${posX}px`;
+        c.style.top = `${posY}px`;
+    })
+}
