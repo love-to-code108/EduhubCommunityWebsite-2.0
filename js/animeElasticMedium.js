b714414f172c92c30f7f253b6cde6c8f01b7b301
love-to-code108 <love.to.code108@gmail.com>
2023-07-02 19:40:41 +0530

    modified animeElasticMedium.js file

---
+++ js/animeElasticMedium.js
@@ -0,0 +1,73 @@
+let animeMed = gsap.timeline();
+
+animeMed
+.to("#div19",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+})
+.to("#div20",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+},"<")
+.to("#div21",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+},"<")
+.to("#div22",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+},"<")
+.to("#div23",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+},"<")
+.to("#div24",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+},"<")
+.to("#div25",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+},"<")
+.to("#div26",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+},"<")
+.to("#div27",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+},"<")
+.to("#div28",{
+    delay:3.9,
+    ease:"elastic",
+    duration:2,
+    x: -350,
+    y: -348.85
+},"<")
