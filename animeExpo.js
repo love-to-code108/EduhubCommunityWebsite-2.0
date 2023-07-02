b8763ef951d94931cd665a465ef3d2a8721d98f6
love-to-code108 <love.to.code108@gmail.com>
2023-07-02 19:00:19 +0530

    added two seperate files for elastic and expo

---
+++ animeExpo.js
@@ -0,0 +1,191 @@
+
+
+let anime = gsap.timeline();
+
+anime.
+// =======================================
+// the animation begins
+
+from("#firstelm" ,{
+    y :-5000,
+    ease:"power4",
+    duration:1.8})
+// =======================================
+// the rotation starts
+.to("#firstelm" ,{
+    rotation: 360*20,
+    borderRadius: "50%",
+    scale:0.8,
+    duration:3,
+},"<0.9")
+// =======================================
+// the small particles prepare
+
+// plotted div1
+.to("#div1",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:-300,
+    y:+44.44,
+})
+
+// plotted div2
+.to("#div2",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:-700,
+    y:0,
+},"<")
+// plotted div3
+.to("#div3",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:-700,
+    y:+222.22,
+},"<")
+
+// plotted div4
+.to("#div4",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:-400,
+    y:+444.4,
+},"<")
+
+// plotted div5
+.to("#div5",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:+0,
+    y:+333.32,
+},"<")
+
+// plotted div6
+.to("#div6",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:-900,
+    y:-111.11,
+},"<")
+
+// plotted div7
+.to("#div7",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:-700,
+    y:-222.2,
+},"<")
+
+// plotted div8
+.to("#div8",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:-400,
+    y:-444.4,
+},"<")
+
+// plotted div9
+.to("#div9",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:-300,
+    y:-166.66,
+},"<")
+
+// plotted div10
+.to("#div10",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:0,
+    y:-333.32,
+},"<")
+
+// plotted div11
+.to("#div11",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:+400,
+    y:+444.4,
+},"<")
+
+// plotted div12
+.to("#div12",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x: +600,
+    y: 0,
+},"<")
+
+// plotted div13
+.to("#div13",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x: +300,
+    y: +111.1,
+},"<")
+
+// plotted div14
+.to("#div14",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:+900,
+    y: +111.11,
+},"<")
+
+// plotted div15
+.to("#div15",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:+600,
+    y: 0,
+},"<")
+
+// plotted div16
+.to("#div16",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:+300,
+    y:-166.65,
+},"<")
+
+// plotted div17
+.to("#div17",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x: 700,
+    y: -222.2,
+},"<")
+
+// plotted div18
+.to("#div18",{
+    scale:1.2,
+    duration:2,
+    ease:"expo",
+    x:+400,
+    y:-444.4,
+},"<")
+
+// ==============================================
+// the explosion effect
+.to("#firstelm" ,{
+    scale: 1.4,
+    ease:"expo",
+    duration:0.8,
+},"<")
